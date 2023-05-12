183f5a6fd02abd3c23686fe0cb97e2a286339d79
aidanlhbaros <aidanlhbaros@gmail.com>
2023-05-11 21:25:18 -0600

    mostly done

--- button.py
+++ button.py
@@ -2,16 +2,23 @@ import pygame
 from pygame import Vector2
 
 class Button:
-    def __init__(self, title:str, pos:Vector2, size:Vector2, color:tuple[int,int,int]):
+    def __init__(self, title:str, pos:Vector2, size:Vector2, button_color:tuple[int,int,int], text_color:tuple[int,int,int]):
         self.title:str = title
         self.pos:Vector2 = pos
         self.size:Vector2 = size
-        self.color:tuple[int,int,int] = color
-        self.rect = pygame.Rect(int(self.pos.x),int(self.pos.y),int(self.size.x), int(self.size.y))
+        self.button_color:tuple[int,int,int] = button_color
+        self.text_color:tuple[int,int,int] = text_color
+        self.rect:pygame.Rect = pygame.Rect(0,0,int(self.size.x), int(self.size.y))
+        self.rect.center = self.pos
+        self.font = pygame.font.SysFont("Roboto", int(self.size.y))
+        self.text = self.font.render(self.title, True, text_color)
+        self.text_rect = self.text.get_rect()
+        self.text_rect.center = self.rect.center
 
 
     def draw(self, screen:pygame.surface.Surface):
-        pygame.draw.rect(screen,self.color,self.rect)
+        pygame.draw.rect(screen,self.button_color,self.rect)
+        screen.blit(self.text, self.text_rect)
 
 
     def collide(self,mouse_pos:tuple[int,int]):
--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import pygame
 from pygame.math import Vector2
 import random
 from tiles import Tile
+from button import Button
 import time
 
 
 class Game:
     def __init__(self):
+        self.playing = True
         pygame.init()
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.screen:pygame.surface.Surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.screen_size:Vector2 = Vector2(self.screen.get_size())
-        self.clock = pygame.time.Clock()
+        self.clock:pygame.time.Clock = pygame.time.Clock()
         self.tile_images:str = "tile_images.png"
-        self.flag = pygame.image.load(self.tile_images)
-        self.grid_size:Vector2 = Vector2(18,18)
-        self.max_num_mines = int(self.grid_size.x * self.grid_size.y * 0.5)
-        self.num_mines:int = 100
+        self.flag:pygame.image = pygame.image.load(self.tile_images)
+        self.grid_size:Vector2
+        self.num_mines:int
         self.center_on_x:bool
         self.x_offset:int = int(self.screen_size.x * 0.05)
         self.y_offset:int = int(self.screen_size.y * 0.05)
@@ -24,94 +25,48 @@ class Game:
             Vector2(-1,0),Vector2(0,0),Vector2(1,0),
             Vector2(-1,1),Vector2(0,1),Vector2(1,1)
         ]
-        if self.screen_size.x/self.grid_size.x < self.screen_size.y/self.grid_size.y:
-            self.tile_size = int((self.screen_size.x*0.9)/self.grid_size.x)
-            self.center_on_x = False
-            self.y_offset = int((self.screen_size.y - (self.grid_size.y * self.tile_size))/2)
-        else:
-            self.tile_size = int((self.screen_size.y*0.9)/self.grid_size.y)
-            self.center_on_x = True
-            self.x_offset = int((self.screen_size.x - (self.grid_size.x * self.tile_size))/2)
-        self.grid:list[list[Tile]] = []
-        self.create_grid()
-        self.start_tick = pygame.time.get_ticks()
-        self.font = pygame.font.SysFont("Arial", 40)
-        self.flag = pygame.transform.scale(self.flag,(int(self.tile_size/4)*13,int(self.tile_size/4)))
+        self.font = pygame.font.SysFont("Roboto", int(self.screen_size.y/27))
+        self.flag = pygame.transform.scale(self.flag,(int(self.screen_size.y * 0.04)*13,int(self.screen_size.y * 0.04)))
 
-        self.mouse_check = False
+        self.grid:list[list[Tile]] = []
+        self.start_tick:int = 0
+        self.mouse_check:bool = False
         self.Running:bool = True
         self.first_click:bool = False
         self.queue:list[Tile] = []
-        self.num_flags = 0
+        self.num_flags:int = 0
+        self.in_UI:bool = True
+        self.button_list:list[Button] = []
+        self.seconds:int = 0
+        self.last_time:str =  "---"
 
-        
-        
 
     def run(self):
-        while self.Running:
-            self.clock.tick(60)
-            self.num_flags = 0
-            for row in self.grid:
-                for tile in row:
-                    if tile.flaged:
-                        self.num_flags += 1
-            for event in pygame.event.get():
-                mouse_press = pygame.mouse.get_pressed()
-                if event.type == pygame.QUIT:
-                    self.Running = False
-                if event.type == pygame.MOUSEBUTTONDOWN and not self.mouse_check:
-                    self.mouse_check = True
-                    if mouse_press[0] == 1:
-                        self.left_click()
-                    elif mouse_press[1] == 1:
-                        self.middle_click()
-                    elif mouse_press[2] == 1 and self.first_click:
-                        self.right_click()
-                if event.type == pygame.MOUSEBUTTONUP:
-                    self.mouse_check = False
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LCTRL]:
-                self.Running = False
-            
-            seconds = round((pygame.time.get_ticks() - self.start_tick) / 1000)
-            print(seconds)
-            self.win_check(seconds)
-            self.loose_check()
-
-            self.reveal()
-
-
-            self.screen.fill((0,100,0))
-            for row in self.grid:
-                for tile in row:
-                    tile.draw(self.screen)
-            
-            self.screen.blit(
-                self.flag,
-                (self.x_offset + (self.tile_size*self.grid_size.x) - self.tile_size/4,(self.screen_size.y*0.01)),
-                (int((self.tile_size/4)*7),0,int(self.tile_size/4),int(self.tile_size/4))
-                )
-            self.screen.blit(
-                self.font.render(str(self.num_mines - self.num_flags), True, "white"),
-                (self.x_offset + (self.tile_size*self.grid_size.x) - (self.tile_size/4)*2.5,(self.screen_size.y*0.005))
-                )
-            self.screen.blit(
-                self.font.render(str(seconds), True, "white"), 
-                (self.x_offset,(self.screen_size.y*0.005))
-                )
-
-            pygame.display.flip()
+        while self.playing:
+            if self.in_UI:
+                self.UI()
+            self.game()
+            self.grid:list[list[Tile]] = []
+            self.start_tick:int = 0
+            self.mouse_check:bool = False
+            self.Running:bool = True
+            self.first_click:bool = False
+            self.queue:list[Tile] = []
+            self.num_flags:int = 0
+            self.in_UI:bool = True
+            self.button_list:list[Button] = []
+            self.seconds:int = 0
 
 
     def left_click(self):
         breakflag = False
-        mouse = pygame.mouse.get_pos()
         for y, row in enumerate(self.grid):
             for x, tile in enumerate(row):
-                if tile.hitbox.collidepoint(mouse):
+                if tile.hitbox.collidepoint(self.mouse_pos):
                     if not tile.flaged:
                         if not self.first_click:
                             self.first_click = True
+                            self.start_tick = pygame.time.get_ticks()
                             for i in self.tiles_around:
                                 if self.offscreen(tile.grid_pos,i):
                                     self.grid[int(y+i.y)][int(x+i.x)].cant_be_mine = True
@@ -253,8 +208,9 @@ class Game:
                 if not tile.is_mine and tile.clicked:
                     check += 1
         if not_mine == check:
-            print(f"Your time was {seconds} seconds")
+            self.last_time = str(seconds)
             self.Running = False
+            self.in_UI = True
 
 
     def loose_check(self):
@@ -263,7 +219,134 @@ class Game:
                 if tile.is_mine and tile.clicked:
                     print("YOU LOOSE")
                     self.Running = False
+                    self.in_UI = True
+
+
+    def edge_display(self):
+        self.screen.blit(
+            self.flag,
+            (self.x_offset + (self.tile_size*self.grid_size.x) - self.screen_size.y * 0.04,(self.screen_size.y*0.01)),
+            (int((self.screen_size.y * 0.04)*7),0,int(self.screen_size.y * 0.04),int(self.screen_size.y * 0.04))
+            )
+        text = self.font.render(str(self.num_mines - self.num_flags), True, "white")
+        self.screen.blit(
+            text,
+            (self.x_offset + (self.tile_size*self.grid_size.x) - ((text.get_width()) + (self.screen_size.y * 0.04)),(self.screen_size.y*0.01))
+            )
+        self.screen.blit(
+            self.font.render(f"Time: {self.seconds}", True, "white"), 
+            (self.x_offset,(self.screen_size.y*0.01))
+            )
+
+
+    def update(self):
+        self.mouse_pos:tuple[int,int] = pygame.mouse.get_pos()
+        self.num_flags = 0
+        for row in self.grid:
+            for tile in row:
+                if tile.flaged:
+                    self.num_flags += 1
+        self.seconds = round((pygame.time.get_ticks() - self.start_tick) / 1000)
+        if not self.first_click:
+            self.seconds = 0
+        self.win_check(self.seconds)
+        self.loose_check()
+
+        self.reveal()
+
+        self.screen.fill((0,100,0))
+        for row in self.grid:
+            for tile in row:
+                tile.draw(self.screen)
+        
+        self.edge_display()
+
+
+    def start_up(self):
+        self.max_num_mines:int = int(self.grid_size.x * self.grid_size.y * 0.5)
+        if self.screen_size.x/self.grid_size.x < self.screen_size.y/self.grid_size.y:
+            self.tile_size = int((self.screen_size.x*0.9)/self.grid_size.x)
+            self.center_on_x = False
+            self.y_offset = int((self.screen_size.y - (self.grid_size.y * self.tile_size))/2)
+        else:
+            self.tile_size = int((self.screen_size.y*0.9)/self.grid_size.y)
+            self.center_on_x = True
+            self.x_offset = int((self.screen_size.x - (self.grid_size.x * self.tile_size))/2)
+        self.create_grid()
+
+
+    def game(self):
+        while self.Running:
+            self.clock.tick(60)
+            for event in pygame.event.get():
+                mouse_press = pygame.mouse.get_pressed()
+                if event.type == pygame.QUIT:
+                    self.quit()
+                if event.type == pygame.MOUSEBUTTONDOWN and not self.mouse_check:
+                    self.mouse_check = True
+                    if mouse_press[0] == 1:
+                        self.left_click()
+                    elif mouse_press[1] == 1:
+                        self.middle_click()
+                    elif mouse_press[2] == 1 and self.first_click:
+                        self.right_click()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.mouse_check = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LCTRL] or keys[pygame.K_ESCAPE]:
+                self.quit()
+            
+            self.update()
+
+            pygame.display.flip()
 
 
     def UI(self):
-        pass
+        easy = Button("Easy",Vector2(self.screen_size.x/2 - (self.screen_size.x * 0.25),self.screen_size.y/1.25),Vector2(self.screen_size.x * 0.2,self.screen_size.y * 0.1),(0,190,0),(0,0,0))
+        self.button_list.append(easy)
+        medium = Button("Medium",Vector2(self.screen_size.x/2,self.screen_size.y/1.25),Vector2(self.screen_size.x * 0.2,self.screen_size.y * 0.1),(0,190,0),(0,0,0))
+        self.button_list.append(medium)
+        hard = Button("Hard",Vector2(self.screen_size.x/2 + (self.screen_size.x * 0.25),self.screen_size.y/1.25),Vector2(self.screen_size.x * 0.2,self.screen_size.y * 0.1),(0,190,0),(0,0,0))
+        self.button_list.append(hard)
+
+        while self.in_UI:
+            self.mouse_pos:tuple[int,int] = pygame.mouse.get_pos()
+            self.clock.tick(60)
+            for event in pygame.event.get():
+                mouse_press = pygame.mouse.get_pressed()
+                if event.type == pygame.QUIT:
+                    self.quit()
+                if event.type == pygame.MOUSEBUTTONDOWN and not self.mouse_check:
+                    self.mouse_check = True
+                    if mouse_press[0] == 1:
+                        for button in self.button_list:
+                            if button.collide(self.mouse_pos):
+                                if button == easy:
+                                    self.grid_size = Vector2(10,8)
+                                    self.num_mines = 10
+                                if button == medium:
+                                    self.grid_size = Vector2(18,14)
+                                    self.num_mines = 40
+                                if button == hard:
+                                    self.grid_size = Vector2(24,20)
+                                    self.num_mines = 100
+                                self.start_up()
+                                self.in_UI = False
+                                self.Running = True
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.mouse_check = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LCTRL] or keys[pygame.K_ESCAPE]:
+                self.quit()
+
+            self.screen.fill((0,100,0))
+            for button in self.button_list:
+                button.draw(self.screen)
+
+            pygame.display.flip()
+
+
+    def quit(self):
+        self.Running = False
+        self.in_UI = False
+        self.playing = False
